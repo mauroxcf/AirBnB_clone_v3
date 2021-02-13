ac309f82ceb3b24ab9d0b83c22eb3d7635d836bb
mauroxcf <maurox03@gmail.com>
2021-02-12 20:21:11 -0500

    checking if we get a connection with the API HBNB

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,61 @@
+const listAmenities = [];
+
+$(document).ready(function () {
+  getStatus();
+  $('input[type="checkbox"]').change(checkboxChange);
+});
+
+/**
+ * checkboxChange - adds or deletes the choises amenities in h4 of amenities
+ */
+function checkboxChange () {
+  const amenityName = $(this).data('name');
+
+  if (!this.checked) {
+    // searchs the index in the list if not is in the list return -1
+    const index = listAmenities.indexOf(amenityName);
+
+    if (index !== -1) {
+      /*
+      * first arguments is the index to start to delete
+      * and the second is how many elements going to delete
+      */
+      listAmenities.splice(index, 1);
+    }
+
+    showListAmenities();
+    return;
+  }
+
+  listAmenities.push(amenityName);
+
+  showListAmenities();
+}
+
+/**
+ * showListAmenities - to check if the list is empty
+ * if is empty put &nbsp; in the HTML
+ */
+function showListAmenities () {
+  if (listAmenities.length) {
+    $('div .amenities H4').text(listAmenities.join(', '));
+  } else {
+    $('div .amenities H4').html('&nbsp;');
+  }
+}
+
+/**
+ * getStatus - remove or add the class available
+ * access to the status code to do that.
+ */
+function getStatus () {
+  $.get('http://localhost:5001/api/v1/status/',
+    function (data, status) {
+      if (data.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    }
+  );
+}
